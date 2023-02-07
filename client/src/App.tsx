5461c0c932d1c2bb96a3fa655c44969a71572a0e
zhengx55 <williamzhxin@gmail.com>
2023-02-07 12:08:50 +0800

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return <div className="font-semibold bg-red-50">2</div>;
+}
+
+export default App;
